5c6743cbe0eeb0f3c82fba568606c51a1692feaa
mattdo98 <54378681+mattdo98@users.noreply.github.com>
2019-12-08 16:05:21 -0500

    Updated main, has methods to light up each pin

--- main.asm
+++ main.asm
@@ -19,15 +19,54 @@ jmp start
 .org 0x1000
 start:
 
-;setup port J pin 
+;setup port J (named port C in manual) pin 
 lds	r16,PORTC_DIR		;Store Port J to r16
 ldi	r16,0b00000001		;turn bit 0 on
-sts	PORTC_DIR,r16		;store r16 bacj to port J
+sts	PORTC_DIR,r16		;store r16 back to port J
 
-ldi r16, 0b00000001		
+;First light turning on
+ldi r16, 0b00000001
 sts PORTC_OUT, r16
+;First light turning off
 ldi	r16, 0b00000000
 sts PORTC_OUT,r16
+
+;Second light turning on
+ldi r16, 0b00000010
+sts PORTC_OUT, r16
+;Second light turning off
+ldi	r16, 0b00000000
+sts PORTC_OUT,r16
+
+;Third light turning on
+ldi r16, 0b00000100
+sts PORTC_OUT, r16
+
+;Third light turning off
+ldi	r16, 0b00000000
+sts PORTC_OUT,r16
+
+;Fourth light turning on
+ldi r16, 0b00001000
+sts PORTC_OUT, r16
+;Fourth light turning off
+ldi	r16, 0b00000000
+sts PORTC_OUT,r16
+
+;Fifth light turning on
+ldi r16, 0b00010000
+sts PORTC_OUT, r16
+;Fifth light turning off
+ldi	r16, 0b00000000
+sts PORTC_OUT,r16
+
+;Sixth light turning on
+ldi r16, 0b00100000
+sts PORTC_OUT, r16
+;Sixth light turning off
+ldi	r16, 0b00000000
+sts PORTC_OUT,r16
+
 	
 end:	rjmp end
 
